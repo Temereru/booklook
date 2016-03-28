988a6ed5a02daa4343878735bd47b46e6b83d5f4
Tomer David <younrg@gmail.com>
2016-03-28 16:50:05 +0200

    finished

--- js/script.js
+++ js/script.js
@@ -1,42 +1,67 @@
+$('#loading-image').hide();
 (function($) {
    'use strict';
    var source   = $("#entry-template").html();
    var template = Handlebars.compile(source);
 
-   var renderHtml = function (book) {
-      var newHtml = template(book);
+   var renderHtml = function (books) {
+      var newHtml = template(books);
+      console.log(books);
+      console.log(newHtml);
       $('.books').append(newHtml);
    };
 
    var getData = function (data) {
-      var book = {
-         title: '',
-         description: '',
-         authors: [],
-         url: '',
-         pageCount: ''
-      };
-      $(data.items[0].volumeInfo).each(function (idx, val) {
-         book.title = val.title;
-         book.description = val.description;
-         $(val.authors).each(function (i, v) {
-            book.authors.push(v);
+      // console.log(data);
+      var books = [];
+      $(data.items).each(function (id, value){
+         var book = {
+            title: '',
+            description: '',
+            authors: [],
+            url: '',
+            pageCount: ''
+         };
+         $(value.volumeInfo).each(function (idx, val) {
+            book.title = val.title;
+            book.description = val.description;
+            $(val.authors).each(function (i, v) {
+               book.authors.push(v);
+            });
+            book.url = val.imageLinks.thumbnail;
+            book.pageCount = val.pageCount;
          });
-         book.url = val.imageLinks.thumbnail;
-         book.pageCount = val.pageCount;
+         books.push(book);
       });
-      console.log(book);
-      return book;
+      
+      // console.log(books);
+      return books;
    };
 
-   var fetch = function (isbn) {
+   // var fetch = function (isbn) {
+   //    $.ajax({
+   //       method: "GET",
+   //       url: 'https://www.googleapis.com/books/v1/volumes?q='+isbn,
+   //       dataType: "json",
+   //       success: function(data) {
+   //          var book = getData(data);
+   //          renderHtml(book);
+   //          // console.log(book);
+   //          // console.log(data);
+   //       },
+   //       error: function(jqXHR, textStatus, errorThrown) {
+   //          console.log(textStatus);
+   //       }
+   //    });
+   // }
+      var fetch = function (name) {
       $.ajax({
          method: "GET",
-         url: 'https://www.googleapis.com/books/v1/volumes?q='+isbn,
+         url: 'https://www.googleapis.com/books/v1/volumes?q='+name+'&maxResults=10',
          dataType: "json",
          success: function(data) {
-            var book = getData(data);
-            renderHtml(book);
+            var books = getData(data);
+            renderHtml({books:books});
             // console.log(book);
             // console.log(data);
          },
@@ -48,9 +73,18 @@
 
    $('button[type="search"]').on('click', function (e) {
       e.preventDefault();
-      var valIsbn = $('#isbn').val();
+      var valTitle = $('#name').val();
+      valTitle = valTitle.replace(' ','+');
       $('.books').empty();
-      fetch(valIsbn);
-      $('#isbn').val('').focus();
+      fetch(valTitle);
+      $('#name').val('').focus();
    });
 }(jQuery));
+
+$(document).ajaxStart( function(){
+   console.log('started');
+    $('#loading-image').show();
+});
+$(document).ajaxStop( function(){
+    $('#loading-image').hide();
+});
